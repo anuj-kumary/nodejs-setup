d63c584d263dc29a9fb63aaf47078a47a962a637
anuj-kumary <anujf0510@gmail.com>
2025-01-11 00:06:29 +0530

    Added basic connection for mongoDb setup

---
+++ monogoDB-setup/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello, World! This is your TypeScript server.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
