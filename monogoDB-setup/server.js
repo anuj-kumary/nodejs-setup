6ea511a5dc33301eefca633f588039ac95cc6e9c
anuj-kumary <anujf0510@gmail.com>
2025-01-12 11:23:11 +0530

    Add mongoDB package verify get call

--- monogoDB-setup/server.js
+++ monogoDB-setup/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! This is your TypeScript server.");
+    res.send("Hello, World! This is your MongoDB server.");
 });
 
 app.listen(PORT, () => {
